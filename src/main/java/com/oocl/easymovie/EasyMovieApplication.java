9343bfdd661afe9182d4fca6b0004d78e4b515a2
Edward Chen <hi@edch.top>
2022-08-06 14:02:00 +0800

    chore: init project

---
+++ src/main/java/com/oocl/easymovie/EasyMovieApplication.java
@@ -0,0 +1,13 @@
+package com.oocl.easymovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyMovieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyMovieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oocl/easymovie/EasyMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oocl.easymovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyMovieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
