93112bbd45c03f952bce8934878060bfac5e7e92
Edward Chen <hi@edch.top>
2022-08-06 15:13:05 +0800

    feat: add prefix config and cors config

---
+++ src/main/java/com/oocl/easymovie/config/WebMvcConfig.java
@@ -0,0 +1,28 @@
+package com.oocl.easymovie.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.method.HandlerTypePredicate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author edward
+ */
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.addPathPrefix("api", HandlerTypePredicate.forAnnotation(RestController.class));
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOrigins("*");
+    }
+
+}
