a4225c9975b62bea701a390f6a42fd6382b09233
Edward Chen <hi@edch.top>
2022-08-07 23:29:43 +0800

    feat: add entity `Order` and `Schedule`

---
+++ src/main/java/com/oocl/easymovie/entity/Schedule.java
@@ -0,0 +1,36 @@
+package com.oocl.easymovie.entity;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+/**
+ * @author edward
+ */
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Schedule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long movieId;
+    private Long theaterId;
+    private Date startTime;
+    private Date endTime;
+    private Double price;
+    @CreationTimestamp
+    private Date createTime;
+    @UpdateTimestamp
+    private Date updateTime;
+}
