70d28a6de58100e0869c8dd247e1e37958d2be9e
Edward Chen <hi@edch.top>
2022-08-07 23:44:08 +0800

    feat: add entity `Comment` and `PurchasePoint`

---
+++ src/main/java/com/oocl/easymovie/entity/PurchasePoint.java
@@ -0,0 +1,35 @@
+package com.oocl.easymovie.entity;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+/**
+ * @author edward
+ */
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class PurchasePoint {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long userId;
+    private Integer balance;
+    private Integer historyTotal;
+
+    @CreationTimestamp
+    private Date createTime;
+    @UpdateTimestamp
+    private Date updateTime;
+}
