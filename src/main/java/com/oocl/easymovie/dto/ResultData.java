0fc312992422b90be8d76c8d33ff9f0f4c14df09
Edward Chen <hi@edch.top>
2022-08-08 11:45:29 +0800

    feat: update response JavaBean

--- src/main/java/com/oocl/easymovie/dto/ResultData.java
+++ src/main/java/com/oocl/easymovie/dto/ResultData.java
@@ -19,10 +19,4 @@ public class ResultData<T> {
     return resultData;
   }
 
-  public static <T> ResultData<T> fail(String message) {
-    ResultData<T> resultData = new ResultData<>();
-    resultData.setStatus(message);
-    return resultData;
-  }
-
 }
