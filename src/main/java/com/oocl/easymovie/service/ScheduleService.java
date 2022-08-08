5aba5ee5f0e22b426dc4ba8fdc6517466911f215
Carlos Luo <15576412155@163.com>
2022-08-08 20:56:46 +0800

    feat: add /schedule?theaterId={theaterId}&movieId={movieId} api

---
+++ src/main/java/com/oocl/easymovie/service/ScheduleService.java
@@ -0,0 +1,20 @@
+package com.oocl.easymovie.service;
+
+import com.oocl.easymovie.entity.Schedule;
+import com.oocl.easymovie.repository.ScheduleRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ScheduleService {
+    private final ScheduleRepository scheduleRepository;
+
+    public ScheduleService(ScheduleRepository scheduleRepository) {
+        this.scheduleRepository = scheduleRepository;
+    }
+
+    public List<Schedule> findScheduleByTheaterIdAndMovieId(Long theaterId, Long movieId) {
+        return scheduleRepository.findAllByTheaterIdAndMovieId(theaterId, movieId);
+    }
+}
