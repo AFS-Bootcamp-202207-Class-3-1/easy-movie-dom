964a81e4235ad8a0b959f577097fbecbb4db0001
MELANIE ZHOU (ITA-ISDC-ISD-OOCLL/ZHA) <ZHOUME@OOCL.COM>
2022-08-10 10:40:50 +0800

    test : build and pass the movie controller test

---
+++ src/test/java/com/oocl/easymovie/controller/ScheduleControllerTest.java
@@ -0,0 +1,15 @@
+package com.oocl.easymovie.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class ScheduleControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+}
