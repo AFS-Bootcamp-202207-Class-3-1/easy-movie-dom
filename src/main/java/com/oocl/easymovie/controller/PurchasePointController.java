6068e094bdcd54b1adbb4bd53d5fc06e67769100
Carlos Luo <15576412155@163.com>
2022-08-10 14:05:07 +0800

    fix: update purchasePoint url

--- src/main/java/com/oocl/easymovie/controller/PurchasePointController.java
+++ src/main/java/com/oocl/easymovie/controller/PurchasePointController.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/charge")
+@RequestMapping("/purchasePoint")
 public class PurchasePointController {
     @Autowired
     private PurchasePointService purchasePointService;
