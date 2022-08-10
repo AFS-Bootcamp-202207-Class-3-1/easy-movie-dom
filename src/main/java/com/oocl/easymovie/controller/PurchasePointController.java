233aa1ec80b69df5d10955e41ff882c112a60ee5
Carlos Luo <15576412155@163.com>
2022-08-10 12:35:38 +0800

    fix: sorry

---
+++ src/main/java/com/oocl/easymovie/controller/PurchasePointController.java
@@ -0,0 +1,19 @@
+package com.oocl.easymovie.controller;
+
+import com.oocl.easymovie.dto.ResultData;
+import com.oocl.easymovie.entity.Schedule;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/charge")
+public class PurchasePointController {
+    /*@GetMapping()
+    ResultData<List<Schedule>> () {
+        return
+    }*/
+}
