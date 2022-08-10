acfe9d84c8ec76720bd4c8ce64985edfd48589f4
Carlos Luo <15576412155@163.com>
2022-08-10 15:33:53 +0800

    fix: recharge user purchase point

--- src/main/java/com/oocl/easymovie/util/ParseCode.java
+++ src/main/java/com/oocl/easymovie/util/ParseCode.java
@@ -5,12 +5,13 @@ import com.oocl.easymovie.exception.CodeIllegalException;
 public class ParseCode {
 
     public static final int CODE_LENGTH = 3;
-    public static final String PRE_CODE = "coffeee";
+    public static final String PRE_CODE = "coffee";
     public static final String SUF_CODE = "studio";
+    public static final String CODE_REGEX = "-";
 
     public static int parseCode(String code) {
-        String[] split = code.split("-");
-        if (split.length != CODE_LENGTH || PRE_CODE.equals(split[0]) || SUF_CODE.equals(split[2])) {
+        String[] split = code.split(CODE_REGEX);
+        if (split.length != CODE_LENGTH || !PRE_CODE.equals(split[0]) || !SUF_CODE.equals(split[2])) {
             throw new CodeIllegalException();
         }
         try {
