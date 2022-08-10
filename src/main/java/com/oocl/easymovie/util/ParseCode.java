bdf1337c082371fef50d6de6933c92e034546e56
Carlos Luo <15576412155@163.com>
2022-08-10 15:10:17 +0800

    feat: add seating table and verify recharge bu user id api

--- src/main/java/com/oocl/easymovie/util/ParseCode.java
+++ src/main/java/com/oocl/easymovie/util/ParseCode.java
@@ -1,8 +1,22 @@
 package com.oocl.easymovie.util;
 
+import com.oocl.easymovie.exception.CodeIllegalException;
+
 public class ParseCode {
+
+    public static final int CODE_LENGTH = 3;
+    public static final String PRE_CODE = "coffeee";
+    public static final String SUF_CODE = "studio";
+
     public static int parseCode(String code) {
         String[] split = code.split("-");
-        return Integer.parseInt(split[1]);
+        if (split.length != CODE_LENGTH || PRE_CODE.equals(split[0]) || SUF_CODE.equals(split[2])) {
+            throw new CodeIllegalException();
+        }
+        try {
+            return Integer.parseInt(split[1]);
+        } catch (NumberFormatException e) {
+            throw new CodeIllegalException();
+        }
     }
 }
