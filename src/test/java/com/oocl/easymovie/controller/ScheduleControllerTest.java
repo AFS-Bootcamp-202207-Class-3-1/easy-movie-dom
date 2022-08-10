123a2c6af10a41234d6b163ecc4feb3944a2efa8
MELANIE ZHOU (ITA-ISDC-ISD-OOCLL/ZHA) <ZHOUME@OOCL.COM>
2022-08-10 10:54:26 +0800

    test : build and pass schedule controller test

--- src/test/java/com/oocl/easymovie/controller/ScheduleControllerTest.java
+++ src/test/java/com/oocl/easymovie/controller/ScheduleControllerTest.java
@@ -1,10 +1,13 @@
 package com.oocl.easymovie.controller;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -12,4 +15,20 @@ import org.springframework.test.web.servlet.MockMvc;
 public class ScheduleControllerTest {
     @Autowired
     private MockMvc mockMvc;
+
+    @Test
+    void should_return_schedule_when_perform_get_given_theaterId_and_movieId() throws Exception {
+        //given
+
+        //when
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/schedule")
+                .param("theaterId","1")
+                .param("movieId","1"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data[0].theaterId").value(1))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data[0].movieId").value(1));
+
+        //then
+
+    }
 }
